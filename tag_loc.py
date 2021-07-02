58225142cc4893910a6cd24127ffdfae46911399
gkuhn100 <55503972+gkuhn100@users.noreply.github.com>
2021-07-01 20:54:33 -0600

    Update tag_loc.py
    Tag with 'APG' command

--- tag_loc.py
+++ tag_loc.py
@@ -7,13 +7,12 @@ This is a temporary script file.
 
 import serial
 import datetime
+import sys
 import time
 from sense_hat import SenseHat
 import numpy as np
 
-init = False
-con_sec = 0
-time_s = 0
+
 sense = SenseHat()
 
 
@@ -24,9 +23,15 @@ if ser.isOpen:
     time.sleep(1)
     line = ser.write("\r\r".encode())
     time.sleep(1)
-    ser.write("lec\r".encode())
     
-def get_Accel():
+def get_pos():
+    ser.write('apg\r'.encode())
+    line = ser.readline()
+    parse = line.decode().split()
+    print(parse)
+    
+    
+def get_accel():
     accel = sense.get_accelerometer_raw()
     X = accel['x']
     Y = accel['y']
@@ -38,30 +43,8 @@ def get_Accel():
     
 
 while True:
-    line=ser.readline()
-    #print(line)
-    if(line):
-        if len(line) > 140 and 'POS'.encode() in line:
-            con_sec+= 1
-            parse = line.decode().split(",")
-            x_pos = parse[parse.index("POS")+1]
-            y_pos = parse[parse.index("POS")+2]
-            z_pos = parse[parse.index("POS")+3]
-            qf = parse[parse.index("POS")+4]
-            accel = get_Accel()
-            accel_x = accel[0]
-            accel_y  = accel[1]
-            #if init == 1:
-            if (con_sec >=3 or init):
-                time_now = time.strftime("%H:%M:%S")
-                print("At time " + time_now + "  The tag is at position {0} {1}" 
-                      .format(x_pos,y_pos)  )
-                print("And is accelerating at {0:.5f} m/s^2 in the X and {1:.5f} in the "
-                      "Y" .format(accel_x,accel_y))
-                time.sleep(1)
-                init = True
-        else:
-            con_sec = 0
-    #print("Consecutuve iterations {}" .format(con_sec))
-            
-             
+   time_now= time.strftime("%H:%M:%S")
+   print(time_now)
+   get_pos()
+   accel = get_accel()
+   time.sleep(1)
