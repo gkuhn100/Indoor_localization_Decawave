aeabb72c9f2bec2134e5a2a2331a0ac48990eb28
gkuhn100 <55503972+gkuhn100@users.noreply.github.com>
2021-07-17 20:52:43 -0600

    Revised  f d zgn

--- tag_loc.py
+++ tag_loc.py
@@ -21,6 +21,7 @@ q = np.array([[.001,.001],[.002,.002]])
 H = np.array([[1,0],[0,1]])
 R = np.array([[.05,0],[0,.05]])
 C = np.array([[1,0],[0,1]])
+I = np.array([[1,0],[0,1]])
 
 
 ser =  serial.Serial('/dev/ttyACM0',115200, timeout = 1)
@@ -30,7 +31,7 @@ if ser.isOpen:
     time.sleep(1)
     line = ser.write("\r\r".encode())
     time.sleep(1)
-    
+
 def get_pos():
     ser.write('apg\r'.encode())
     line = ser.readline()
@@ -43,7 +44,7 @@ def get_pos():
         parse = parse[1:]
         return(parse)
     else:
-        return()   
+        return()
 def get_accel():
     accel = sense.get_accelerometer_raw()
     X = accel['x']
@@ -51,14 +52,14 @@ def get_accel():
     Z = accel['z']
     accel_list = [X,Y,Z]
     return(accel_list)
-    
+
 def predict_state(parse,accel_list):
     tag_loc = np.array([[parse[0].strip('x:')],[parse[1].strip('y:')]],dtype=float)
     accel = np.array([[accel_list[0]],[accel_list[1]]],dtype=float)
     X_est = np.dot(A,tag_loc) + np.dot(B,accel)
     print('And the state estimate is {0}'.format(X_est))
     return(X_est)
-    
+
 def process_cov():
     Pc= np.array([[.05,.05],[.05,.05]])
     Pc = np.dot(A,Pc)
@@ -79,20 +80,29 @@ def Observation(parse):
     obs = np.dot(C,tag_loc)
     return(obs)
 
-   
-    
+def Current(X_est,Kg,obs):
+    X_num = obs - np.dot(Y,X_est)
+    X_current = X_est+np.dot(Kg,X_num)
+    return(X_current)
+
+def UpdateProcess(Pc,Kg):
+    Pcu = I - np.dot(K,H)
+    Pc = np.dot(Pcu,Pc)
+    return(Pc)
+
 while True:
-   time_now= time.strftime("%H:%M:%S")   
+   time_now = time.strftime("%H:%M:%S")
    tag_pos = get_pos()
    accel = get_accel()
-   
-   
+
+
    if tag_pos:
        print('At time {0} the tag is at location {1} and is acellerating at {2}m/s^2'.format(time_now,tag_pos,accel))
        loc = predict_state(tag_pos,accel)
        pc = process_cov()
        kg = KalmanGain(loc,pc)
        obs = Observation(tag_pos)
-       
+
+
        print("\n")
    time.sleep(.5)
