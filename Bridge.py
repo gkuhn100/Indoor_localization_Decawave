b75497403dcf817d8b4cfd908e93613265e51b3b
Gregory Kuhn <55503972+gkuhn100@users.noreply.github.com>
2021-08-26 21:50:43 -0600

    Update Bridge.py

--- Bridge.py
+++ Bridge.py
@@ -9,20 +9,32 @@ DWM.write("\r\r".encode())
 time.sleep(1)
 DWM.write("lec\r".encode())
 time.sleep(1)
+time_delta = 0
+count = 0
+Init = False
 while True:
     try:
         line=DWM.readline()
         if(line):
             if len(line)>=140:
+                count +=1
                 parse=line.decode().split(",")
                 x_pos=parse[parse.index("POS")+1]
                 y_pos=parse[parse.index("POS")+2]
+                qf = parse[parse.index("POS")+4]
                 val = (x_pos,y_pos)
-                print(datetime.datetime.now().strftime("%H:%M:%S"),"(",x_pos,", ",y_pos,")")
+                print('At time ' + datetime.datetime.now().strftime("%H:%M:%S"),"(",x_pos,", ",y_pos,")")
+                if count > 5:
+                    Init = True
             else:
                 print("Position not calculated: ",line.decode())
+                count = 0
     except Exception as ex:
         print(ex)
         break
+
 DWM.write("\r".encode())
 DWM.close()
+
+
+
