98ac7e88382b9bfa54f27187a87810a956e46cc6
Gregory Kuhn <55503972+gkuhn100@users.noreply.github.com>
2021-08-30 21:21:45 -0600

    Update Anchor.py

--- Anchor.py
+++ Anchor.py
@@ -7,11 +7,19 @@ Created on Sat Aug 28 21:57:16 2021
 """
 
 line = "DIST,4,AN0,0E15,1.75,4.55,2.33,1.61,AN1,1516,0.00,4.55,2.20,2.15,AN2,41B1,1.75,0.00,2.20,4.23,AN3,089F,0.00,0.00,2.20,4.37,POS,1.49,4.02,0.78,59"
-
+count = 0
+Anchor_list = []
 if line.find("DIST") != -1:
     Line = line.split(",")
+    numb_anchor = int(Line[Line.index("DIST")+1])
     for place,item in enumerate(Line):
         if item.find("AN") !=-1:
-            print(Line[place])
+            Anch_name = item
             
-        
+
+
+
+
+for i in range(24):
+    Anchor_list.append(Line[i+2])
+
