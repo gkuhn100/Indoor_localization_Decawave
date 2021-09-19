5e7b343cc01a2a24b7a465c8e394048f17cd9c49
Gregory Kuhn <55503972+gkuhn100@users.noreply.github.com>
2021-09-18 20:17:14 -0600

    Update Bridge.py

--- Bridge.py
+++ Bridge.py
@@ -9,24 +9,22 @@ DWM.write("\r\r".encode())
 time.sleep(1)
 DWM.write("lec\r".encode())
 time.sleep(1)
-init = False
 count = 0
+init = False
 pc = time.time_ns()
 
 def print_anchor(Line):
     line = Line.decode('ascii')
-    Anch_name = []
+    Anch_name =  []
     Anch_place = []
-
-    if line.find("DIST") != -1:
-        Line = line.split(",")
-        num_anchor = int(Line[Line.index("DIST")+1])
-    for place,item in enumerate(Line):
+    line = line.split(",")
+    num_anchor = int(line[line.index("DIST")+1])       
+    for place,item in enumerate(line):
         if item.find("AN") !=-1:
             Anch_name.append(item)
             Anch_place.append(place)
 
-    print("There are {0} anchors in this Setup".format(num_anchor))
+    print('There are {0} Anchors ' .format(num_anchor))
     for i in range(len(Anch_place)):
         print("Anchor {0} is named {1} At located at {2} {3} {4}".format(Anch_name[i],Line[Anch_place[i]+1],Line[Anch_place[i]+2],Line[Anch_place[i]+2],Line[Anch_place[i]+3]))
 
@@ -39,26 +37,25 @@ def time_delta():
     print("The name elapsed in nano seconds is {0} milli is {1} and in seconds is {2} ".format(dt,dt_m,dt_s ))
     pt = tc
     return(dt_s)  
-
+pt = time.time_ns()
+print(pt)
 while True:
     try:
         line=DWM.readline()
-        print_anchor(line)
         if(line):
             if len(line)>=140:
+                tc= time.time_ns()
+                dt = tc -pt
+                pt = tc
+                #print_anchor(line)
+                count +=1
+                print(count)
                 parse=line.decode().split(",")
                 x_pos=parse[parse.index("POS")+1]
                 y_pos=parse[parse.index("POS")+2]
                 qf = parse[parse.index("POS")+4]
                 val = (x_pos,y_pos)
-                Dt = time_delta()
                 print('At time ' + datetime.datetime.now().strftime("%H:%M:%S"),"(",x_pos,",",y_pos,")")
-                count +=1
-             
-                if count == 5 and Dt<5:
-                   init = True
-              
-                
             else:
                 print("Position not calculated: ",line.decode())
     except Exception as ex:
@@ -67,5 +64,4 @@ while True:
 
 DWM.write("\r".encode())
 DWM.close()
-
 
