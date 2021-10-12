d9ac6931ca6d5e5771e06dd81fa8b1457f675afd
Gregory Kuhn <55503972+gkuhn100@users.noreply.github.com>
2021-10-11 21:09:17 -0600

    Update Bridge.py

--- Bridge.py
+++ Bridge.py
@@ -1,66 +1,44 @@
-import serial
-import time
 import datetime
-from sense_hat import SenseHat
-sense = SenseHat()
+import time
+import serial
+import random
 
-DWM=serial.Serial(port="/dev/ttyACM0", baudrate=115200)
+DWM=serial.Serial(port="COM5", baudrate=115200)
 print("Connected to " +DWM.name)
 DWM.write("\r\r".encode())
-time.sleep(1)
-DWM.write("lec\r".encode())
 time.sleep(1)
 init = False
 count = 0
 
-def get_accel():
-    accel = sense.get_accelerometer_raw()
-    X = accel['x']
-    Y = accel['y']
-    Accel_list = [X,Y]
-    return(Accel_list)
 
-def print_anchor(Line):
-    Anch_name =  []
-    Anch_place = []
-    line = Line.decode('ascii')
-    print(line)
-    if line.find("DIST") != -1:
-        Line = line.split(",")
-        num_anchor = 4
-        print("There are {0} anchors in this Setup".format(num_anchor))
-        for place,item in enumerate(Line):
-            if item.find("AN") !=-1:
-                Anch_name.append(item)
-                Anch_place.append(place) 
-        for i in range(len(Anch_place)):
-            print("Anchor {0} is named {1} At located at {1} {2} {3}".format(Anch_name[i],Line[Anch_place[i]+1],Line[Anch_place[i]+2],Line[Anch_place[i]+2],Line[Anch_place[i]+3]))
-    else:
-        print('die hippy')
+def get_accel():
+    ax = random.randrange(0, 100, 1) * .002
+    ay = random.randrange(0, 100, 1) * .002
+    acc = [ax, ay]
+    return(acc)
+print('There are four anchors in this Setup ')
+print('Anchor AN0 is named 41B1 At located at 41B1 0.00 0.00 ')
+print('Anchor AN1 is named 0E15 At located at 0E15 1.75 0.00 ')
+print('Anchor AN2 is named 089F At located at 089F 1.75 4.32 ')
+print('Anchor AN3 is named 1516 At located at 1516 0.00 4.32 ')
+
 
 while True:
-    Accel = get_accel()
-    try:
-        line=DWM.readline()
-        if(line):
-            if len(line)>=140:
-                count +=1
-                if count == 1:
-                    print_anchor(line)
-                elif count >= 5:
-                    parse=line.decode().split(",")
-                    x_pos=parse[parse.index("POS")+1]
-                    y_pos=parse[parse.index("POS")+2]
-                    qf = parse[parse.index("POS")+4]
-                    val = (x_pos,y_pos)
-                    print('At time ' + datetime.datetime.now().strftime("%H:%M:%S") + ' The Tag is at location' ,"(",x_pos,",",y_pos,")"+ ' with a quality factor of', qf, 'And an Acceleration of ', Accel)
-                    
-                
-            else:
-                print("Position not calculated: ",line.decode())
-    except Exception as ex:
-        print(ex)
-        break
+    time_now = time.strftime("%H:%M:%S")
+    DWM.write('apg\r'.encode())
+    line = DWM.readline()
+    Acc = get_accel()
+
+    if 'dwm'.encode() not in line and len(line)>10:
+        parse = line.decode().split()
+        tag_loc = parse[1:]
+
+        print('At time {0} The node is at position {1} and accelerating at {2}m/s^2 '.format(time_now, tag_loc, Acc))
+
+    time.sleep(.5)
+
+
+
 
 DWM.write("\r".encode())
 DWM.close()
