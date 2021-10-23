78f661692bc4cc2011266ac2ec5a065d4cd1e059
Gregory Kuhn <55503972+gkuhn100@users.noreply.github.com>
2021-10-23 14:38:07 -0600

    Update DWM.py

--- DWM.py
+++ DWM.py
@@ -4,8 +4,7 @@ import datetime
 #from sense_hat import SenseHat
 #sense = SenseHat()
 
-
-DWM = serial.Serial(port="COM5", baudrate=115200)
+DWM = serial.Serial(port="COM5", baudrate=115200, timeout=1)
 print('Connected to ' + DWM.name)
 DWM.write("\r\r".encode())
 time.sleep(1)
@@ -13,13 +12,22 @@ DWM.write("lec\r".encode())
 time.sleep(1)
 init = False
 count = 0
+stat =  0
+
 
 def get_accel():
     accel = sense.get_accelerometer()
     X = accel['X']
     Y = accel['Y']
     Accel_list = [X,Y]
-    return Accel_list
+    return (Accel_list)
+
+def det_motion(Accel_list):
+    X_accel = Accel_list[0]
+    Y_accel = Accel_list[1]
+
+def print_kalman(Acce_list,tag_loc):
+    print("position")
 
 def print_anchor(Line):
     Anch_name =  []
@@ -41,27 +49,38 @@ def print_anchor(Line):
 
 while True:
     #Accel=get_accel()
-    try:
-        line=DWM.readline()
-        if(line):
-            if len(line)>=140:
-                count +=1
-                if count == 1:
-                    print_anchor(line)
-                elif count >= 5:
-                    parse=line.decode().split(",")
-                    x_pos=parse[parse.index("POS")+1]
-                    y_pos=parse[parse.index("POS")+2]
-                    qf = parse[parse.index("POS")+4]
-                    val = (x_pos,y_pos)
-                    print('At time ' + datetime.datetime.now().strftime("%H:%M:%S") + ' The Tag is at location' ,"(",x_pos,",",y_pos,")"+ ' with a quality factor of', qf)
+    line=DWM.readline()
+    if(line):
+        print('Count is {0}'.format(count))
+        if len(line)>=140:
+            count +=1
+            if count == 1:
+                print_anchor(line)
+            if count == 5:
+                init = True
+            if init:
+                stat = 0
+                parse=line.decode().split(",")
+                x_pos=parse[parse.index("POS")+1]
+                y_pos=parse[parse.index("POS")+2]
+                qf = parse[parse.index("POS")+4]
+                val = (x_pos,y_pos)
+                print('At time ' + datetime.datetime.now().strftime("%H:%M:%S") + ' The Tag is at location' ,"(",x_pos,",",y_pos,")"+ ' with a quality factor of', qf)
+        else:
+            print("Position not calculated: ",line.decode())
+    if not line and init == True:
+        print('device is not moving')
+        stat = 1
+    if stat == 1:
+        DWM.write("lec\r".encode())
+        Line = DWM.readline()
+        Line = Line.decode('ascii')
+        time.sleep(.1)
+        if Line.find("APG") != -1:
+            X_pos = float(line[0].strip('x:'))*1e-3
+            Y_pos = float(parse[1].strip('y:'))*1e-3
+            print('device is stationary')
 
-
-            else:
-                print("Position not calculated: ",line.decode())
-    except Exception as ex:
-        print(ex)
-        break
 
 DWM.write("\r".encode())
 DWM.close()
