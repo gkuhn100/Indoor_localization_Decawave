b8c68227b83fd12a043e67b1cbb97cf92c76abbf
gkuhn100 <55503972+gkuhn100@users.noreply.github.com>
2021-11-27 23:59:01 -0600

    adding process covariance and delta_time functions

--- DWM.py
+++ DWM.py
@@ -13,10 +13,11 @@ B = np.array([[.5,0],[.5,0]],dtype=float) # B matrix for converting control matr
 W = np.array([[.05],[0.025]])# Predict State error matrix
 Q = np.array([[.000212],[.04]]) #Error in the Predict State Matrix
 R = np.array([[.05],[.05]]) #Measurment Uncertainty Matrix
-Pc = np.array([[.05,0.0],[0.0,.05]])#Process Uncertainty Matrix
-I = np.array([[1,0],[0,1]]) # Identity Matrix
+I = np.array([[1,0],[0,1]]) #Identity Matrix
 H = np.array([[1,0],[0,1]]) ##Kalman Gain Conversion Matrix
 C = np.array([[1,0],[0,1]]) ##Measurement to Observation matrix
+delta_X = .5 ##Uncertainty in X_position
+delta_Y = .5 ##Uncertainty in Y_position
 
 ## Code to open and establish the serial port connection
 DWM = serial.Serial("/dev/ttyACM0", 115200, timeout = 1)
@@ -31,7 +32,7 @@ init = False
 Temp = False
 count = 0
 
-## The Function below will be used to recieve and return the tsg's acceleration
+## The Function below will be used to recieve and return the tag's acceleration
 def get_accel():
     accel = sense.get_accelerometer_raw()
     X = accel['x']
@@ -65,7 +66,7 @@ def get_tag(Line):
     X_pos = (Line[0].strip('x:'))
     Y_pos = (Line[1].strip('y:'))
     Qf =    (Line[3].strip('qf:'))
-    tag = [X_pos,Y_pos]
+    tag = [X_pos+.05,Y_pos+.05]
     return(tag,Qf)
 
 ## Function to return and print the tag's position from the 'lec' command
@@ -79,11 +80,25 @@ def tag_lec(Line):
             print(tag_pos)
             return(tag_pos)
 
-## Function to predict future State
+## Function to predict the future State
 def predict_state(x_est,Accel_list):
-    Accel = np.array([[Accel_listccel[0]],[Accel_list[1]]],dtype=float)
-    X_est = np.dot(A,est) + np.dot(B,Accel)
+    Accel = np.array([[Accel_list[0]],[Accel_list[1]]],dtype=float)
+    X_est = np.dot(A,est) + np.dot(B,Accel)+np.ndarray(W)
     return(X_est)
+
+def proccess_cov_int():
+    Pc = np.array([[delta_X*delta_X], delta_X*delta_Y], [[delta_Y*delta_X],delta_Y*delta_Y],dtype=float)
+    Pc[0][1] = 0.0
+    Pc[1][0] = 0.0
+    return(Pc)
+
+def proccess_cov(Pc):
+    Pc = np.dot(A,Pc)
+    Pc = np.dot(Pc,At)+Q
+    Pc[0][1] = 0.0
+    Pc[1][0] = 0.0
+    return(Pc)
+
 
 ## Displays an error signal if one of the anchor node is malfunctioning
 def anchor_error(line,num_anchor):
@@ -98,6 +113,8 @@ def anchor_error(line,num_anchor):
 
 ## Main Function
 if __name__ == '__main__':
+    dT = 0.0
+    PC = proccess_cov_int()
     while True:
         time_now = datetime.datetime.now().strftime("%H:%M:%S")
         line = DWM.readline()
@@ -106,7 +123,7 @@ if __name__ == '__main__':
         if line.find('DIST') != -1:
             count+=1
             tag_loc=tag_lec(line)
-            print(f'At time {time_now} the tag is at position {tag_loc}')
+            print(f'At time {time_now} the tags estimated position is {tag_loc} and is accelerating at {Accel}m/s^2')
         if count == 1 and not Temp:
             anchor_init=int(print_anchor(line))
             Temp = True
@@ -115,10 +132,16 @@ if __name__ == '__main__':
         if init:
             DWM.write('apg\r'.encode())
             time.sleep(.5)
+            dT = time.time() - dT
             num_anchor=anchor_error(line,anchor_init)
             if line.find("apg") != -1 and len(line)>10:
                 tag_loc,Qf = get_tag(line)
+                est = predict_state(est,Accel)
+                Pc = proccess_cov(Pc)
                 print("At time {0} the Tag's observed location is {1} with a Quality factor of {2} and Accelerating at {3} m/s^2" .format(time_now,tag_loc,Qf,Accel))
-            est = predict_state(ext,Accel)   
+                print('The Predicited State is ')
+                print('The Kalman Gain is ')
+                print('The Updated State is ')
+
 DWM.write("\r".encode())
 DWM.close()
