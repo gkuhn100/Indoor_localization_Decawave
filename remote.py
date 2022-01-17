0f00152277a0a5c6564756d97818b21857d66505
gkuhn100 <55503972+gkuhn100@users.noreply.github.com>
2022-01-16 19:24:06 -0700

    Update remote.py

--- remote.py
+++ remote.py
@@ -6,15 +6,12 @@ import multiprocessing as mp
 from sense_hat import SenseHat
 sense = SenseHat()
 
-## Global Variables
-count = 0
-
 ## Establish a serial coonection
 baudrate = 115200
 port1 = "/dev/ttyACM1"
 port2 = "/dev/ttyACM2"
 tag1 = serial.Serial(port1, baudrate, timeout = 1) ##tag_apg
-tag2 = serial.Serial(port2, baudrate, timeout = 1) ##lec
+tag2 = serial.Serial(port2, baudrate, timeout = 1) ##tag_lec
 time.sleep(1)
 
 ## Tag1 is used for the "apg" command. Solely to get the position of the tag
@@ -49,34 +46,36 @@ def sort_apg(line):
     Line = Line[1:]
     X_pos = (Line[0].strip('x:'))
     Y_pos = (Line[1].strip('y:'))
-    ##Qf =    (Line[3].strip('qf:'))
-    Qf = 67
+    Qf =    (Line[3].strip('qf:'))
+    ##Qf = 75
     tag_apg = [X_pos,Y_pos]
     return(tag_apg, Qf)
 
-
 ## Sorts the Results of the command after "lec" has been entered
 def sort_lec(line):
+    global Count = 0
     line = line.decode()
-    line = line.split()
+    line = line.split(',')
     if len(line) > 10 and line.find('DIST') != -1:
+        Count +=1
         for place,item in enumerate(line):
             if item.find('POS') != -1:
                 pos = place + 1
                 tag_lec = line[pos:]
+                print(tag_lec)
                 return(tag_lec)
     else:
         return(False)
 
 ## Function to determine if the tag node is indeed stationary
 def det_stationary(tag_lec, tag_apg, Accel):
-    if (tag_lec):
+    if (tag_lec == False):
         tag_pos = 1
     else:
         tag_pos = 0.0
     return(tag_pos)
 
-## Function which displays if the position of the anchor
+## Function that displays the position and status of the anchor
 def print_anchor(lec_pos):
     Anch_name =  []
     Anch_place = []
@@ -84,7 +83,7 @@ def print_anchor(lec_pos):
     num_anchor = line[1]
     print(f"There are {num_anchor} Anchors in the setup")
     for place,item in enumerate(line):
-        if item.find != -1:
+        if item.find('POS') != -1:
             Anch_name.append(item)
             Anch_place.append(place)
     for i in range(len(Anch_place)):
@@ -93,13 +92,17 @@ def print_anchor(lec_pos):
     return(num_anchor)
 
 if __name__ == "__main__":
-    count = 0
     dT = 0.0
-    ##tag_lec = tag2.readline()
-    
+    Global Count
+    count = 0
     while True:
         tag_lec = tag2.readline()
-        print(tag_lec)
+        tag_pos_lec = sort_lec(tag_lec)
+        if (tag_pos_lec) != false:
+            count +=1
+        if count == 1:
+            print_anchor()
+            print(f"The count is {Count})
         time_now = datetime.datetime.now().strftime("%H:%M:%S")
         q  = mp.Queue()
         p1 = mp.Process(target = print_apg(q))
@@ -111,6 +114,7 @@ if __name__ == "__main__":
         if len(tag_apg) > 20 and tag_apg.find('apg') != -1:
             tag_loc,qf = sort_apg(tag_apg)
             accel = get_accel()
+            tag_pos = det_stationary(tag_lec, tag_apg, Accel)
             dT = time.time() - dT
             print(f"At time {time_now} the tag estimate is {tag_loc} and accelerating at {accel} m/s^2")
             time.sleep(1)
