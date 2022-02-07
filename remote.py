e348f71204a9265978221e09369edf8c0dba73de
Gregory Kuhn <55503972+gkuhn100@users.noreply.github.com>
2022-02-06 18:32:20 -0700

    Update remote.py
    Flawed but aesthetically pleasing Kalman Display; still need to change something and add the process covariance matrix. 
    Intro is fully complete sans detect if tag is stationary however.

--- remote.py
+++ remote.py
@@ -3,6 +3,7 @@ import serial
 import time
 import datetime
 import multiprocessing as mp
+import numpy as np
 from sense_hat import SenseHat
 sense = SenseHat()
 
@@ -10,6 +11,22 @@ sense = SenseHat()
 count = 0
 init = False
 dT = 0
+delta_X = 0.5 ##Uncertainty in X_position
+delta_Y = 0.5 ##Uncertainty in Y_position
+
+
+## Matrices used in the Kalman Filter
+A = np.array([[1,0],[0,1]]) # matrix for converting state model matrix
+At= np.transpose(A) ## Transpose matrix
+B = np.array([[.5,0],[.5,0]],dtype=float) # B matrix for converting control matrix
+W = np.array([[.05],[0.025]])# Predict State error matrix
+Q = np.array([[.000212],[.04]]) #Error in the Predict State Matrix
+R = np.array([[.05],[.05]]) #Measurment Uncertainty Matrix
+I = np.array([[1,0],[0,1]]) #Identity Matrix
+H = np.array([[1,0],[0,1]]) ##Kalman Gain Conversion Matrix
+C = np.array([[1,0],[0,1]]) ##Measurement to Observation matrix
+Pc = np.array([[(delta_X * delta_X),0.0], [0.0,delta_Y*delta_Y]], dtype=float) ## initiliaize the Process Covariance Matrix
+
 
 ## Establish a serial coonection
 baudrate = 115200
@@ -35,12 +52,36 @@ print('')
 ## Function to print the Acceleration of the tag in the x and y coordinates
 def get_accel():
     accel = sense.get_accelerometer_raw()
-    X = accel['x'] * 9.81
+    X = accel['x'] * 9.81 - 0.062
     X = round(X,3)
-    Y = accel['y'] * 9.81
+    Y = accel['y'] * 9.81 + 0.968
     Y = round(Y,3)
     Accel = [X,Y]
     return(Accel)
+    
+    
+## Sorts the Results of the command after "lec" has been entered
+def sort_lec(tag_lec):
+    global count
+    lec_tag = tag_lec.decode()
+    lec_line = lec_tag.split(',')
+    if len(lec_line) > 10 and lec_tag.find('DIST') != -1:
+        count +=1
+
+    
+## Function which displays if the position of the anchor
+def print_anchor(tag_lec):
+    Anch_name =  []
+    Anch_place = []
+    lec_tag = tag_lec.decode()
+    lec_pos = lec_tag.split(",")
+    num_anchor = int(lec_pos[1])
+    print(f"There are {num_anchor} Anchors in the setup")
+    for place, item in enumerate(lec_pos):
+        if item.find("AN") !=-1:
+            Anch_name.append(item)
+            Anch_place.append(place)
+            print(f"The tag {item} named {lec_pos[place+1]} is at location {lec_pos[place+2]}, {lec_pos[place+3]}, {lec_pos[place+4]}" )
 
 ## enters command 'apg' and gets the results returned from this line
 def print_apg(q):
@@ -56,39 +97,33 @@ def sort_apg(line):
     Y_pos = float((Line[1].strip('y:'))) * 1e-3
     Qf =    (Line[3].strip('qf:'))
     tag_apg = [X_pos,Y_pos]
-    return(tag_apg, Qf)
+    return(tag_apg, Qf) 
 
-## Sorts the Results of the command after "lec" has been entered
-def sort_lec(tag_lec):
-    global count
-    lec_tag = tag_lec.decode()
-    lec_line = lec_tag.split(',')
-    if len(lec_line) > 10 and lec_tag.find('DIST') != -1:
-        count +=1
+## Function to predict the state of the tag based on its previous state estimate and acceleration
+def predict_state(X_est, Accel):
+    X_est = np.dot(A,X_est) + np.dot(B,Accel)
+    return(X_est)
+    
+## Function to calculate the Kalman Gain
+def Kalman_Gain(X_est,Pc):
+    Kg_num = np.dot(Pc,H)
+    Kg_den = np.dot(H,Pc)
+    Kg_den = np.dot(Kg_den,H) + R
+    Kg     = np.divide(Kg_num,Kg_den)
+    Kg[0][1] = 0.0
+    Kg[1][0] = 0.0
+    return(Kg)
 
-## Function which displays if the position of the anchor
-def print_anchor(tag_lec):
-    Anch_name =  []
-    Anch_place = []
-    lec_tag = tag_lec.decode()
-    lec_pos = lec_tag.split(",")
-    num_anchor = int(lec_pos[1])
-    print(f"There are {num_anchor} Anchors in the setup")
-    for place, item in enumerate(lec_pos):
-        if item.find("AN") !=-1:
-            Anch_name.append(item)
-            Anch_place.append(place)
-            print(f"The tag {item} named {lec_pos[place+1]} is at location {lec_pos[place+2]}, {lec_pos[place+3]}, {lec_pos[place+4]}" )
+##Function to update the state estimate; taking in account predicted, measured states and acceleration
+def update_state(X_est,tag_apg,Kg):
+    num = tag_apg - np.dot(H,X_est)
+    X_est = X_est + np.dot(Kg,num)
+    print()
+    return(X_est)
 
-## Function to determine if the tag node is indeed stationary
-def det_stationary(tag_lec, tag_apg, Accel):
-    if (tag_lec):
-        tag_pos = 1
-    else:
-        tag_pos = 0.0
-    return(tag_pos)
 
 if __name__ == "__main__":
+    X_est = np.array([0.0,0.0])
     while True:
         if init == False:
             tag_lec = tag2.readline()
@@ -111,10 +146,12 @@ if __name__ == "__main__":
                 current_time = time.time()
                 tag_loc,qf = sort_apg(tag_apg)
                 accel = get_accel()
-                print(f"At time {time_now} the tag estimate is {tag_loc} and is accelerating at {accel} m/s^2")
-                print(f"The estimated position is ")
-                print(f"The Kalman Gains is ")
-                print("The updated position estimate is")
+                X_est = predict_state(X_est,accel)
+                Kg = Kalman_Gain(X_est,Pc)
+                print(f"At time {time_now} the measured tag positions is {tag_loc} and is accelerating at {accel} m/s^2")
+                print(f"The estimated position is {X_est} ")
+                X_est = update_state(X_est,tag_loc,Kg)
+                print(f"And an updated state estimate of {X_est}")
                 time.sleep(1)
                 dT = round((time.time() - current_time),3)
                
