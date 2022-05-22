d0c40bdd5e2043d27f8f392aa8eb82832caa8c6d
Gregory Kuhn <55503972+gkuhn100@users.noreply.github.com>
2022-05-21 18:51:13 -0600

    Update tag_lab_tes03.py

--- tag_lab_tes03.py
+++ tag_lab_tes03.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 """
+Created on Sat May 21 16:21:51 2022
+
+@author: Gregory Kuhn
+"""
+# -*- coding: utf-8 -*-
+"""
 Created on Sun May 15 11:30:35 2022
 Python Script to Measure Decawave Tag; and improve /
-results with K-filters
+results with K-filter
 @author: Gregk
 """
-
 
 """Importing modules and libraries """
 import serial
 import time
-import datetime
+from datetime import datetime
 import numpy as np
-import multiprocessing as mp
+##import multiprocessing as mp
 
 """ Below are the arrays that will be useful for Kalman Filter"""
 A = np.array([[1,0],[0,1]]) # A matrix for converting state model
@@ -27,7 +32,7 @@ H = np.array([[1,0],[0,1]]) ##Kalman Gain Conversion Matrix
 C = np.array([[1,0],[0,1]]) ##Measurement to Observation matrix
 
 """Code below is used to establish a serial connection between listner """
-port = "COM4"
+port = "COM10"
 baudrate = 115200
 ser = serial.Serial(port, baudrate, timeout=1)
 
@@ -40,9 +45,48 @@ if ser.isOpen():
 """ the function below is used  to print the position of the tag nodes"""
 def print_tag_loc():
     ser.write('lec\r'.encode())
-    line = ser.readline()
-    return(line)
+    ser.write('av\r'.encode())
+    tag_loc = ser.readline()
+    return(tag_loc)
     
-    if __name__ == "__main__":
-        tag_postion = 
-    
+""" The function below splits the tag location """
+def split_tag_loc(tag_loc):
+    if len(tag_loc) >=30:
+        tag_pos = tag_loc.decode('utf-8')
+        tag_pos = tag_pos.split(',')
+        return(tag_pos)
+    else:
+        return None
+
+def parse_tag_loc(tag_pos):
+    tag_data  =  []
+    tag_name  =  []
+    tag_x_loc =  []
+    tag_y_loc =  []
+    tag_qf    =  []
+    if tag_pos == None:
+        return None
+    else:    
+        for place,item in enumerate(tag_pos):
+            if item.find('POS') != -1:
+                tag_name.append(tag_pos[place+2])
+                tag_x_loc.append(tag_pos[place+3])
+                tag_y_loc.append(tag_pos[place+4])
+                tag_qf.append(tag_pos[place+6])
+                z = [tag_name, tag_x_loc, tag_y_loc, tag_qf]
+                tag_data.append(z)
+                return(tag_data)
+
+if __name__ == "__main__":
+    while (1):
+        try:
+            date = datetime.now().strftime("%H:%M:%S")
+            tag_loc = print_tag_loc()
+            tag_pos = split_tag_loc(tag_loc)
+            tag_data = parse_tag_loc(tag_pos)
+            if (tag_data != None):
+                print(f"At time {date} the tag is at position {tag_data}")
+            time.sleep(1)
+        except KeyboardInterrupt:
+            ser.close()
+            
