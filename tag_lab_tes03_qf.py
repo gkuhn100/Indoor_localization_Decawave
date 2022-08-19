fc7671a97bc57f97842f2ff83f0c26fd064c1646
Gregory Kuhn <55503972+gkuhn100@users.noreply.github.com>
2022-08-18 23:30:53 -0600

    Update tag_lab_tes03_qf.py

--- tag_lab_tes03_qf.py
+++ tag_lab_tes03_qf.py
@@ -11,7 +11,6 @@ import serial
 import time
 import datetime
 import numpy as np
-import csv
 from sense_hat import SenseHat
 sense = SenseHat()
 np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
@@ -39,7 +38,7 @@ delta_X = .05 # Initial Uncertaintity for x position
 delta_Y = .05 # Initial Uncertaintity for y position
 G = 9.8065 # Converting Gforce to m/s^2
 tag_loc_list = []# list to contain all the observed tag_loc; useful in determining if tag is stationary
-
+Qf_list = [] #list which contains all the qualtity factor values
 
 """ Establish a serial connection between tag and Pi """
 baudrate = 115200
@@ -61,7 +60,7 @@ def get_accel():
     Accel_list = [X,Y]
     return(Accel_list)
 
-""" Retuns the orientation of X,Y coordinates in radians/s"""
+""" Retuns the orientation of X,Y coordinates in radians/s """
 def get_gyro():
     gyro = sense.get_gyroscope_raw()
     X = round(gyro['x'], 5)
@@ -72,67 +71,62 @@ def get_gyro():
 # f/n print_tag_pos()]
 """ This function enters the command apg in the tag node and returns
 the decoded value of the line provided the length of the returned values
-is greater than 20. If three successful tags have been detected then the global variable
-init is set to True
+is greater than 20. If so iteration is "iterated" by one
  """
 # return line (decoded into ascii values)
 def print_tag_pos():
-    global count
-    global init
     global Qf
     ser.write("apg\r".encode())
     line = ser.readline()
     line = line.decode('utf-8')
-    if len(line) > 20 and (line.find("x") !=-1) and line.find("y") and Qf > 0:
-        count +=1
-        if count == 10: # changing count to 10 from 3
-            init = True
+    if len(line) > 20 and (line.find("x") !=-1) and line.find("y"):
         return line
-
-# f/n tag_decode(line)
-"""
-only if the length is greater than 20
-returns the observed,parsed,adjusted position of the tag node
-Additionally iterates the iterat variable everytime it is run
-"""
-# input line
-# return tag_loc
-def tag_decode(line):
-    global init
-    global iterat
-    global stat
-    global tag_loc_list
-
-    Line = line.split()
-    Line = Line[1:]
-    X_pos = round(float((Line[0].strip('x:'))) * 1e-3 + .05,4)
-    Y_pos = round(float((Line[1].strip('y:'))) * 1e-3 + .05,4)
-    tag_loc  = [X_pos, Y_pos]
-    if init == True:
-        iterat +=1
-    return tag_loc
-
+    else:
+        return None
+    
+    
 # f/n sort_qf(line)
 """
 only if the length is greater than 20
 returns the qualitfy factor, qf
 """
 # input line
-# return qf
 def sort_qf(line):
     global NLOS
     global Qf
-    global init
-    Qf_list = []
+    global iterat
+    global Qf_list
     Line = line.split(" ")
     Line = Line[1:]
     Qf = int((Line[3].strip('qf:')))
     Qf_list.append(Qf)
-    if init == True:
-        if (Qf_list[iterat]) and (Qf_list[iterat-1]) and (Qf_list[iterat-2])  == 0:
+    if iterat > 10:
+        if (Qf_list[iterat-1]) and (Qf_list[iterat-2]) and (Qf_list[iterat-3])  == 0:
             NLOS = True
+            print('Warning! Tag has passed out of the LOS')
         else:
             NLOS = False
+
+# f/n tag_decode(line)
+"""
+only if the length is greater than 20
+returns the observed,parsed,adjusted position of the tag node
+"""
+# input line
+# return tag_loc
+def tag_decode(line):
+    global Qf
+    global iterat
+    Line = line.split()
+    Line = Line[1:]
+    X_pos = round(float((Line[0].strip('x:'))) * 1e-3 + .05,4)
+    Y_pos = round(float((Line[1].strip('y:'))) * 1e-3 + .05,4)
+    tag_loc  = [X_pos, Y_pos]
+    if Qf != 0:
+        iterat+=1
+    #print(Qf)
+    return tag_loc
+
 
 # f/n predict_state(X_est,Accel)
 """
@@ -146,8 +140,6 @@ def predict_state(X_est, Accel):
     global dT
     B = np.array([[.5*(dT*dT),0],[0,.5*(dT*dT)]],dtype=float) # B matrix for converting control matrix matrix
     X_est = np.dot(A,X_est) + np.dot(B,Accel)
-    X_est[0][1] = 0.0
-    X_est[1][0] = 0.0
     return(X_est)
 
 """
@@ -211,8 +203,6 @@ Kalman Gain. Note remeber the difference between predicted and observed state
 def update_state(X_est,tag_apg,Kg):
     num = tag_apg - np.dot(H,X_est)
     X_est = X_est + np.dot(Kg,num)
-    X_est[0][1] = 0.0
-    X_est[1][0] = 0.0
     return(X_est)
 
 # f/n update_Pc(Pc,Kg)
@@ -240,23 +230,23 @@ if __name__ == "__main__":
                 tag_loc = tag_decode(tag_pos) # Decodes and ouputs X,Y coordinate provide tag_pos is valied
                 tag_loc_list.append(tag_loc)
                 print(f"At time {time_now}, iteration {iterat} the tag is at observed position {tag_loc} and accelerating at {accel}m/s^2 with a quality factor of {Qf}")
-                if iterat == 1 and (NLOS == False):# Used to set the initial tag_location
+                if iterat == 10 and (NLOS == False):# Used to set the initial tag_location
                     Pc = init_cov()
                     delta_t = [time.time()]
                     X_est = tag_loc
-                elif iterat > 1:
+                elif iter == 10 and NLOS == True:
+                    iterat = 0
+                elif iterat > 10:
                     delta_t.append(time.time())
-                    dT = round(delta_t[iterat-1] - delta_t[iterat - 2],4)
+                    dT = round(delta_t[iterat-10] - delta_t[iterat - 11],4)
                     X_est = predict_state(X_est,accel)
                     Pc = predict_cov(Pc)
                     print(f"The predicted position is {X_est} with a process covariance of {Pc}")
                     Kg = kalman_gain(X_est,Pc)
-                    X_est = update_state(X_est,tag_loc_list[iterat],Kg)
+                    X_est = update_state(X_est,tag_loc_list[iterat-iterat],Kg)
                     Pc = update_PC(Pc,Kg)
                     print(f"The kalman gain is {Kg} and the updated position is {X_est} with a updated pc of {Pc} and dt of {dT}")
         except KeyboardInterrupt:
-            file_write(Tag_data)
             print('Error! Keyboard interrupt detected, now closing ports! ')
             ser.close()
-            write_file()
         time.sleep(.5)
